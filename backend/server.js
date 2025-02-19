caf2cf7084eb7bf6d5b30e2804bfe39528e7ebb6
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-02-19 23:19:17 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const shopOrderRouter = require("./routes/shop/order-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 const app = express();
 const port = 5000;
@@ -32,6 +33,7 @@ app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/address", shopAddressRouter);
+app.use("/api/shop/review", shopReviewRouter);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
