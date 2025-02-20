959a3d1c20471c095b38dd4f79b027b795ebe8b8
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-02-20 19:23:23 +0530

    cors headres added

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,21 @@ mongoose
   .then(() => console.log("connected"))
   .catch((error) => console.log(error));
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
