bbf4d14cdc03a60079a3bd0d1a9ef234db738b00
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-02-28 17:50:46 +0530

    store updated

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
+import adminOrderSlice from "./admin/order-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
 
     adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
   },
 });
 
