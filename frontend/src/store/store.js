07840f49a59486ab5b367a541ae379d4bfeb0e5f
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-03-02 23:08:42 +0530

    store updated

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -6,6 +6,7 @@ import adminOrderSlice from "./admin/order-slice";
 import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
+import shopOrderSlice from "./shop/order-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -16,6 +17,7 @@ const store = configureStore({
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
+    shopOrder: shopOrderSlice,
   },
 });
 
