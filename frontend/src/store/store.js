103ddce1fd369eedd415a2eff919daad52a075af
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-03-02 23:16:37 +0530

    store added

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
+
 import adminProductsSlice from "./admin/products-slice";
 import adminOrderSlice from "./admin/order-slice";
 
@@ -9,6 +10,7 @@ import shopAddressSlice from "./shop/address-slice";
 import shopOrderSlice from "./shop/order-slice";
 import shopSearchSlice from "./shop/search-slice";
 import shopReviewSlice from "./shop/review-slice";
+import commonFeatureSlice from "./common-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -22,6 +24,8 @@ const store = configureStore({
     shopOrder: shopOrderSlice,
     shopSearch: shopSearchSlice,
     shopReview: shopReviewSlice,
+
+    commonFeature: commonFeatureSlice,
   },
 });
 
