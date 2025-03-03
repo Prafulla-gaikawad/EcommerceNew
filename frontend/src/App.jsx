60dada5e39b921ad63a2891020e5ec509a408a09
Prafulla-gaikawad <159248731+Prafulla-gaikawad@users.noreply.github.com>
2025-03-03 21:47:36 +0530

    app.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useSelector(
+    (state) => state.auth
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+
+  console.log(isLoading, user);
   return (
     <>
-        <h1>hello world</h1>
+      <h1>hello world</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
